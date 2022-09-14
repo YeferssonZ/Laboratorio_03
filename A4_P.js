f0b74bba0dd5c79da5340de3dfa8f74fd89bb44c
Yefersson Guillermo <yefersson.zuniga@tecsup.edu.pe>
2022-09-13 22:36:48 -0500

    Tercera version

---
+++ A4_P.js
@@ -0,0 +1,6 @@
+var nombre = "Yefersson";
+var apellido = "Zuniga";
+var edad = 19;
+ 
+var Mi_nombre = "Mi nombre es " + nombre + " " + apellido + " y tengo " + edad + " años.";
+console.log(Mi_nombre);
---
+++ A7.js
@@ -0,0 +1,17 @@
+let a = 3, b = 2;
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+z = a * b;
+console.log("Resultado de la multiplicación: " + z);
+
+z = a / b;
+console.log("Resultado de la división: " + z)
+
+z = a % b;
+console.log("Resultado de la operación modulo (residuo): " + z);
+z = a ** b;
+console.log("Resultado del operador exponente: " + z);
